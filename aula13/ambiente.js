deaf186ac0d769193ccdca392f4f5b527e666271
Renato Roberto <renato.roberto.ajunior@gmail.com>
2026-02-09 17:48:30 -0300

    Introdução as aulas método de repetições, "while", "do while" e "for"

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6)*/
+
+//-----------------
+
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
