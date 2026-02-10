1137f16c014568e6d06f10a45f0895160fce3f9c
Renato Roberto <renato.roberto.ajunior@gmail.com>
2026-02-10 20:56:13 -0300

    Adicionar estrutura inicial de arquivos para exercícios de JavaScript, incluindo HTML, CSS e script para contagem. Ex da Aula 14. Função de contagem de numeros, com passos numéricos.

---
+++ aula14ex/ex016/scriptAula14.js
@@ -0,0 +1,31 @@
+var inicio = document.getElementById('txtinicio')
+var fim = document.getElementById('txtfim')
+var passo = document.getElementById('txtpasso')
+var res = document.getElementById('res')
+
+function contar() {
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        } else {
+            // Contagem regressiva
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
